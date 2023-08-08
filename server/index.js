417224ab1861d848fab15737eca1b604fc657009
Bishal Changder <changdervishal1999@gmail.com>
2023-08-08 18:17:48 +0530

    .env configured back to original state

--- server/index.js
+++ server/index.js
@@ -7,6 +7,8 @@ const app = express();
 
 // File Paths
 const parentRoute = require("./routes/parentRoute");
+const AppError = require("./utils/appError");
+const globalErrorHandler = require("./controllers/errorController");
 
 // Configuring Middlewares
 dotenv.config();
@@ -14,8 +16,32 @@ app.use(cors());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
+// Sequence of the middlewares are very necessary
+
 // Routing
 app.use("/api/v1/", parentRoute);
+
+// Unhandled routes
+app.all("*", (req, res, next) => {
+  //#region  -- OLD WAY OF DOING
+  // res.status(404).json({
+  //   status: "fail",
+  //   message: `Can't find ${req.originalUrl} on the server`,
+  // });
+
+  // const err = new Error(`Can't find ${req.originalUrl} on the server`);
+  // err.status = "fail";
+  // err.statusCode = 404;
+
+  // If the next method has an argument no matter what it is , it will be treated as error
+
+  //#endregion -- OLD WAY OF DOING
+  next(new AppError(`Can't find ${req.originalUrl} on the server`, 404));
+});
+
+// The error handling Middlware will be called whenever there will be an argument passed into the next method
+// For bugs  : 4 arguments
+app.use(globalErrorHandler);
 
 // Setup server
 app.listen(process.env.PORT, () => {
