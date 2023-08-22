ff034ab12b62d86bbda94c167360350ae0ee663b
Bishal Changder <changdervishal1999@gmail.com>
2023-08-22 19:08:23 +0530

    New assets added

--- client/src/App.js
+++ client/src/App.js
@@ -1,7 +1,18 @@
 import "./App.css";
+import Login from "./pages/Login/Login_Client";
+import Register from "./pages/Register/Register_Client";
+import MainPage from "./pages/MainPage/MainPage";
+import ShowJobs from "./pages/ShowJobs/ShowJobs";
 
-function App() {
-  return <div className="App"></div>;
-}
+const App = () => {
+  return (
+    <div className="App">
+      {/* <Login /> */}
+      <Register />
+      {/* <MainPage /> */}
+      {/* <ShowJobs /> */}
+    </div>
+  );
+};
 
 export default App;
