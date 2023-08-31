27a0f64510b757d1d3ca76443d49882a83288281
Bishal Changder <changdervishal1999@gmail.com>
2023-08-31 12:14:19 +0530

    Constants added for name attribute of Input Components for better code readability

---
+++ client/src/constants/inputNames.js
@@ -0,0 +1,16 @@
+export const NAME = "name";
+export const EMAIL = "email";
+export const PHONE = "phone";
+export const PASSWORD = "password";
+export const PASSWORDCONFIRM = "passwordConfirm";
+
+/* FOR ADD JOBS */
+export const COMPANY_NAME = "companyName";
+export const LOGO_URL = "logoURL";
+export const JOB_POSITION = "jobPosition";
+export const MONTHLY_SALARY = "monthlySalary";
+export const LOCATION = "location";
+export const DESCRIPTION = "description";
+export const ABOUT_COMPANY = "aboutCompany";
+export const SKILLS_REQUIRED = "skillsRequired";
+export const INFORMATION = "additionalInformation";
