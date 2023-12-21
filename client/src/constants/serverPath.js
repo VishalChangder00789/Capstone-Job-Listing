d895155a6e4d1b7a005514003b27d1d8733780fa
Bishal Changder <changdervishal1999@gmail.com>
2023-12-21 14:55:48 +0530

    Updated Port

--- client/src/constants/serverPath.js
+++ client/src/constants/serverPath.js
@@ -1,6 +1,7 @@
-export const SERVER_BASE_URL = "http://localhost:5000/api/v1";
+export const SERVER_PORT = "5000";
+export const SERVER_BASE_URL = `http://localhost:${SERVER_PORT}/api/v1`;
 export const SERVER_GETJOBS = "/_JOBS";
 export const SERVER_LOGIN = "/_LOGIN";
 export const SERVER_REGISTER = "/_REGISTER";
 export const SKILLS = "/_SKILLS";
-export const SERVER_BASE_URL_SAFE = "https://localhost:5000/api/v1";
+export const SERVER_BASE_URL_SAFE = `https://localhost:${SERVER_PORT}/api/v1`;
