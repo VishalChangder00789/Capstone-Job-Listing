ec1025050ea7fa8be5a3963260fb43f9abc6c7dc
Bishal Changder <changdervishal1999@gmail.com>
2023-12-21 15:12:54 +0530

    Modified Paths

--- client/src/constants/serverPath.js
+++ client/src/constants/serverPath.js
@@ -1,7 +1,8 @@
 export const SERVER_PORT = "5000";
-export const SERVER_BASE_URL = `http://localhost:${SERVER_PORT}/api/v1`;
+// export const SERVER_BASE_URL = `http://localhost:${SERVER_PORT}/api/v1`;
 export const SERVER_GETJOBS = "/_JOBS";
 export const SERVER_LOGIN = "/_LOGIN";
 export const SERVER_REGISTER = "/_REGISTER";
 export const SKILLS = "/_SKILLS";
-export const SERVER_BASE_URL_SAFE = `https://localhost:${SERVER_PORT}/api/v1`;
+// export const SERVER_BASE_URL_SAFE = `https://localhost:${SERVER_PORT}/api/v1`;
+export const SERVER_BASE_URL = `https://job-listing-backend-c3dl.onrender.com/api/v1`;
